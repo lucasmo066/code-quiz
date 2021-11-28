a29f81b8d5be954d1b1845c32fe816b2ede6f977
lucasmo066 <lucasmo066@gmail.com>
2021-11-28 15:50:22 -0500

    added styling to buttons and some question box styling

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// 1. You will have <span> 45 seconds </span> to complete all the questions
+// 2. Once you select your answer, you can't reselect.
+// 3. You can't exit from the quiz while playing.
+// 4. 5 seconds gets subtracted for every incorrect answer
+// 5. Your high score will be saved. Good luck!
+
+// What does HTML stand for?
+// hyper text preprocessor
+// hyper text markup language
+// hyper text multiple language
+// hyper tool multi language
+
+// What does CSS stand for?
+// common style sheet
+// colorful style sheet   
+// computer style sheet
+// cascading style sheet
+
+// what does PHP stand for?
+// hypertext preprocessor
+// hypertext programming
+// hypertext pre-programming
+// hometext preprocessor
+
+// What does SQL stand for?
+// styling question language
+// stylesheet query language
+// statement question language
+// structured query language
+
+// 
+// 
+// 
+// 
+// 
+
+// 
+// 
+// 
+// 
+// 
+
+// 
+// 
+// 
+// 
+// 
+
+// 
+// 
+// 
+// 
+// 
